5b3ea363e1be0c395304e6f693fbee8dcc749854
Nayani Gaonkar <nayani.gaonkar@sjinnovation.com>
2022-12-14 14:34:05 +0530

    Initial Commit

---
+++ StoryTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryTime
+//
+//  Created by SJI-GOA-79 on 14/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
